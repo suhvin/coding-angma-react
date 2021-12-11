f6cbfd6e869de4781969364f10e45721a858754a
suhvin <suh.kiseok.new@gmail.com>
2021-12-11 15:19:14 +0900

    lec2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
+// class는 자바스크립트 예약어이기 때문에 className으로 써줘야 함
+// style은 객체로 전달해야 함
+// background-color X / backgroundColor O
+// JSX에서 텍스트를 쓸 때 중괄호 안에 쓰면 변수나 계산식을 삽입할 수 있다(문자, 숫자만 됨)
+
 function App() {
+  const name = "suhvin";
+  const naver = {
+    name : "네이버",
+    url : "https://naver.com",
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <h1 style={{
+        color : "red",
+        backgroundColor : "green",
+      }}>
+        Hello, {name}!
+        <p>{2+3}</p>
+      </h1>
+      <a href={naver.url}>{naver.name}</a>
     </div>
   );
 }
