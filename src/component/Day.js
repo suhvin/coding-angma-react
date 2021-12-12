53b62026e6b838ec6a25d95465431bb43960ae09
suhvin <suh.kiseok.new@gmail.com>
2021-12-12 13:21:24 +0900

    lec11

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,18 +1,29 @@
-import dummy from "../db/data.json";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Word from "./Word";
 
 export default function Day() {
     
     const {day} = useParams();
-    const wordList = dummy.words.filter(word => word.day === Number(day));
+    //const wordList = dummy.words.filter(word => word.day === Number(day));
+    const [words, setWords] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:3001/words?day=${day}`)
+            .then(res => {
+                return res.json()
+            })
+            .then(data => {
+                setWords(data)
+            });
+    }, [day]);
 
     return (
     <div>
         <h2>Day {day}</h2>
         <table>
             <tbody>
-                {wordList.map(word => (
+                {words.map(word => (
                     <Word word={word} key={word.id}/>
                 ))}
             </tbody>
